576ea67c113d21b0af4f3b50585f9231b9cdeb36
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-14 02:32:22 -0400

    updated index with some testing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import Province from "./province.js";
 import Producer from "./province.js";
+import {strict as assert} from 'assert';
 
 
-export default function callMain(){
-    
+function callMain(){
+    const asia = new Province(sampleProvinceData());
+    assert.equal(asia.shortfall, 5);
     
 }
 
+function sampleProvinceData(){
+    return {
+        name: "Asia",
+        producers: [
+            {name: "Byzantium", cost: 10, production: 9},
+            {name: "Attalia", cost: 12, productin: 10},
+            {name: "Sinope", cost: 10, production: 6}
+        ],
+        demand: 30,
+        price: 20
+    };
+}
 
 
 
